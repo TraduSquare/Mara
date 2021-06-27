54ebf22d8f0507d883d37ca54947b15c8f392b98
Darkmet98 <darkmet98@gmail.com>
2021-06-27 13:15:13 +0200

    Implemented some ps vita checks and process

--- Mara.Lib/Platforms/Vita/Main.cs
+++ Mara.Lib/Platforms/Vita/Main.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,74 @@ namespace Mara.Lib.Platforms.Vita
 {
     public class Main : PatchProcess
     {
-        public Main(string oriFolder, string outFolder, string filePath, string[] mediaId, bool dlc) : base(oriFolder, outFolder, filePath)
+        private string titleIdSelected;
+        public Main(string oriFolder, string outFolder, string filePath) : base(oriFolder, outFolder, filePath)
         {
+        }
+
+
+        private bool CheckTitleId()
+        {
+            var sfo = new Sfo($"{oriFolder}{Path.DirectorySeparatorChar}sce_sys{Path.DirectorySeparatorChar}param.sfo");
+            var titleId = File.ReadAllLines($"{tempFolder}{Path.DirectorySeparatorChar}titleids.txt");
+            foreach (var id in titleId)
+            {
+                if ((string) sfo.SfoValues["TITLE_ID"] == id)
+                {
+                    titleIdSelected = id;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public override (int, string) ApplyTranslation()
+        {
+            var check = CheckGame();
+
+            if (check.Item1 != 0)
+                return check;
+
+            var count = maraConfig.FilesInfo.ListOriFiles.Length;
+            var files = maraConfig.FilesInfo;
+
+            for (int i = 0; i < count; i++)
+            {
+                var oriFile = $"{oriFolder}{Path.DirectorySeparatorChar}{files.ListOriFiles[i]}";
+                var xdelta = $"{tempFolder}{Path.DirectorySeparatorChar}{files.ListXdeltaFiles[i]}";
+                var outFile = files.ListOriFiles[i];
+                if (outFile.StartsWith("GAME"))
+                    outFile = $"{outFolder}{Path.DirectorySeparatorChar}rePatch{Path.DirectorySeparatorChar}{titleIdSelected}{outFile.Substring(4)}";
+                else
+                {
+                    // Check if the DLC exist!
+                    if (!File.Exists(oriFile))
+                        continue;
+
+                    outFile = $"{outFolder}{Path.DirectorySeparatorChar}reAddcont{Path.DirectorySeparatorChar}{titleIdSelected}{outFile.Substring(3)}";
+                }
+                
+                var result = ApplyXdelta(oriFile, xdelta, outFile, files.ListMd5Files[i]);
+
+                if (result.Item1 != 0)
+                    return result;
+            }
+
+            return base.ApplyTranslation();
+        }
+
+        private (int, string) CheckGame()
+        {
+            // Check if the GAME folder exists
+            if (!Directory.Exists(outFolder + Path.DirectorySeparatorChar + "GAME"))
+                return (5, "The GAME folder doesn't exist!");
+
+            // Check the current game
+            if (!CheckTitleId())
+                return (6, "Game invalid!");
+
+            return (0, "");
         }
     }
 }
