88f4d28c267f1826a3f2dfd4910c8e935476398a
D3fau4 <19638408+D3fau4@users.noreply.github.com>
2021-07-04 00:24:46 +0200

    Added RomFS builder

---
+++ Mara.Lib/Platforms/Switch/Romfs.cs
@@ -0,0 +1,24 @@
+﻿using LibHac.Fs;
+using LibHac.FsSystem;
+using LibHac.FsSystem.RomFs;
+using System;
+using System.IO;
+
+namespace Mara.Lib.Platforms.Switch
+{
+    public class Romfs
+    {
+        private IStorage romFs;
+
+        public Romfs(string folder)
+        {
+            var builder = new RomFsBuilder(new LocalFileSystem(folder));
+            this.romFs = builder.Build();
+        }
+
+        public Stream ToStream()
+        {
+            return romFs.AsStream();
+        }
+    }
+}
