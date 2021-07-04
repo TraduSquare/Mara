6ea5a436317ceb0a12ce80e2fdd6103424de6bbb
D3fau4 <19638408+D3fau4@users.noreply.github.com>
2021-07-04 12:22:15 +0200

    Added dump to file option.

--- Mara.Lib/Platforms/Switch/Romfs.cs
+++ Mara.Lib/Platforms/Switch/Romfs.cs
@@ -1,4 +1,5 @@
-﻿using LibHac.Fs;
+﻿using LibHac;
+using LibHac.Fs;
 using LibHac.FsSystem;
 using LibHac.FsSystem.RomFs;
 using System;
@@ -9,16 +10,27 @@ namespace Mara.Lib.Platforms.Switch
     public class Romfs
     {
         private IStorage romFs;
+        private long romFssize;
 
         public Romfs(string folder)
         {
             var builder = new RomFsBuilder(new LocalFileSystem(folder));
             this.romFs = builder.Build();
+            this.romFs.GetSize(out this.romFssize).ThrowIfFailure();
         }
 
         public Stream ToStream()
         {
             return romFs.AsStream();
+        }
+
+        public Result DumpToFile(string path, bool deletefolder = false)
+        {
+            using (FileStream file = new FileStream(path, FileMode.Create, FileAccess.ReadWrite))
+            {
+                romFs.CopyToStream(file, romFssize);
+            }
+            return Result.Success;
         }
     }
 }
