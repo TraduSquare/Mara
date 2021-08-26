587436b925f3ad8c218a891434c2c60f84808c38
Darkmet98 <darkmet98@gmail.com>
2021-08-26 21:05:34 +0200

    Implemented a basic patch generator for mara

--- Mara.Generator/Form1.cs
+++ Mara.Generator/Form1.cs
@@ -1,54 +1,180 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Windows.Forms;
+using Mara.Lib;
+using Newtonsoft.Json;
 
 namespace Mara.Generator
 {
     public partial class Form1 : Form
     {
+        private MaraConfig config;
+        private PatchGenerator generator;
+
         public Form1()
         {
+            config = new MaraConfig()
+            {
+                Platform = MaraPlatform.Generic
+            };
             InitializeComponent();
         }
 
-
-        private string SearchFolder(string title)
+        private void GenerateButton_Click(object sender, EventArgs e)
         {
-            using var fbd = new FolderBrowserDialog()
+            if (config == null)
             {
-                Description = title
-            };
-            var result = fbd.ShowDialog();
-
-            return result == DialogResult.OK ? fbd.SelectedPath : string.Empty;
-        }
-
-        private void oriButton_Click(object sender, EventArgs e)
-        {
-            var result = SearchFolder("Select the folder with original files.");
-            if (!string.IsNullOrWhiteSpace(originalTextBox.Text) && string.IsNullOrWhiteSpace(result))
+                MessageBox.Show("Please follow all steps for generating the patch.",
+                    "Config not generated.",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
-            originalTextBox.Text = result;
-        }
+            }
 
-        private void modifiedButton_Click(object sender, EventArgs e)
-        {
-            var result = SearchFolder("Select the folder with modified files.");
-            if (!string.IsNullOrWhiteSpace(modifiedTextBox.Text) && string.IsNullOrWhiteSpace(result))
+            if (config.Info == null)
+            {
+                MessageBox.Show("Please follow step 2 for generating the patch info.",
+                    "Config info not generated.",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
-            modifiedTextBox.Text = result;
-        }
+            }
 
-        private void outButton_Click(object sender, EventArgs e)
-        {
-            var result = SearchFolder("Select the out folder.");
-            if (!string.IsNullOrWhiteSpace(outTextBox.Text) && string.IsNullOrWhiteSpace(result))
+            if (generator == null)
+            {
+                MessageBox.Show("Please follow step 3 for generating the patch files directories.",
+                    "Config path not generated.",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
-            outTextBox.Text = result;
+            }
+
+            UpdateLog("Generating patch, please wait...");
+            config = generator.GeneratePatch(config);
+
+            var json = JsonConvert.SerializeObject(config, Formatting.Indented);
+            File.WriteAllText($"{generator.OutPath}{Path.DirectorySeparatorChar}info.json", json);
+            UpdateLog("Generating zip file, please wait...");
+            GenerateZip();
+
+            MessageBox.Show("Finished, check the out folder",
+                "Finished",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            UpdateLog("Finished.");
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void informationButton_Click(object sender, EventArgs e)
         {
+            var infoWindow = new FormInfo(config);
 
+            var result = infoWindow.UpdateConfig();
+            if (!result.Item1) return;
+
+            config = result.Item2;
+            UpdateLog("Updated patch information.");
+
+        }
+
+        private void FilesButton_Click(object sender, EventArgs e)
+        {
+            var fileWindow = new FileSelectorForm(generator);
+
+
+            var result = fileWindow.UpdateConfig();
+            if (!result.Item1) return;
+
+            generator = result.Item2;
+            UpdateLog("Updated directories information.");
+        }
+
+        private void patcherButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("WIP, wait for the final version from this generator.",
+                "Not finished.",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            /*
+            var patcherWindow = new PatcherForm();
+
+            patcherWindow.ShowDialog();*/
+        }
+
+        private void Button3ds_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!Button3ds.Checked) return;
+            config.Platform = MaraPlatform.Nintendo3ds;
+            UpdateLog("Selected Nintendo 3DS Platform.");
+        }
+
+        private void buttonGeneric_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!buttonGeneric.Checked) return;
+            config.Platform = MaraPlatform.Generic;
+            UpdateLog("Selected Generic Platform.");
+        }
+
+        private void ButtonSwitch_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!ButtonSwitch.Checked) return;
+            config.Platform = MaraPlatform.NintendoSwitch;
+            UpdateLog("Selected Nintendo Switch Platform.");
+        }
+
+        private void ButtonPsvita_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!ButtonPsvita.Checked) return;
+            config.Platform = MaraPlatform.PlaystationVita;
+            UpdateLog("Selected PlayStation Vita Platform.");
+        }
+
+        private void ButtonPs4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!ButtonPs4.Checked) return;
+            config.Platform = MaraPlatform.Playstation4;
+            UpdateLog("Selected PlayStation 4 Platform.");
+        }
+
+        private void UpdateLog(string text)
+        {
+            logTextBox.AppendText($@"{text}{Environment.NewLine}");
+        }
+
+        private void GenerateZip()
+        {
+            var tempPath = Path.GetTempPath();
+
+            File.WriteAllBytes($"{tempPath}{Path.DirectorySeparatorChar}7za.dll", Properties.Resources.sevenzadll);
+            File.WriteAllBytes($"{tempPath}{Path.DirectorySeparatorChar}7za.exe", Properties.Resources.sevenzaexe);
+            File.WriteAllBytes($"{tempPath}{Path.DirectorySeparatorChar}7zxa.dll", Properties.Resources.sevenxzadll);
+
+            var files = $"\"{generator.OutPath}{Path.DirectorySeparatorChar}info.json\" ";
+            var zip = $"\"{generator.OutPath}{Path.DirectorySeparatorChar}result.zip\" ";
+
+            foreach (var file in config.FilesInfo.ListXdeltaFiles)
+            {
+                files += $"\"{generator.OutPath}{Path.DirectorySeparatorChar}{file}\" ";
+            }
+
+            var generateZip = new ProcessStartInfo();
+            {
+                string arguments =
+                    //$" a -mx9 -tzip {zip}{files}-mx=7 -mm=LZMA";
+                    $" a -mx9 -tzip {zip}-r \"{generator.OutPath}\\.\" -mx=7 -mm=LZMA";
+                generateZip.FileName = $"{tempPath}{Path.DirectorySeparatorChar}7za.exe";
+                generateZip.Arguments = arguments;
+                generateZip.UseShellExecute = false;
+                generateZip.CreateNoWindow = true;
+                generateZip.ErrorDialog = false;
+                generateZip.RedirectStandardOutput = true;
+                Process x = Process.Start(generateZip);
+                x.WaitForExit();
+            }
+
+            File.Delete($"{tempPath}{Path.DirectorySeparatorChar}7za.dll");
+            File.Delete($"{tempPath}{Path.DirectorySeparatorChar}7za.exe");
+            File.Delete($"{tempPath}{Path.DirectorySeparatorChar}7zxa.dll");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            logTextBox.Text = string.Empty;
         }
     }
 }
---
+++ Mara.Generator/FormInfo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mara.Lib;
+using Mara.Lib.Configs;
+
+namespace Mara.Generator
+{
+    public partial class FormInfo : Form
+    {
+        private MaraConfig configPassed;
+        private bool savedChanges;
+        public FormInfo(MaraConfig config)
+        {
+            InitializeComponent();
+            configPassed = config;
+
+            if (configPassed.Info != null)
+            {
+                ChangelogTextBox.Text = configPassed.Info.Changelog;
+                PatchIdNumeric.Value = configPassed.Info.PatchId;
+                PatchVersionTextBox.Text = configPassed.Info.PatchVersion;
+            }
+        }
+
+        public (bool, MaraConfig) UpdateConfig()
+        {
+            ShowDialog();
+
+            return (savedChanges, configPassed);
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            configPassed.Info ??= new PatchInfo();
+
+            configPassed.Info.PatchId = (int) PatchIdNumeric.Value;
+            configPassed.Info.Changelog = ChangelogTextBox.Text;
+            configPassed.Info.PatchVersion = PatchVersionTextBox.Text;
+            savedChanges = true;
+            Close();
+        }
+    }
+}
