1b80ce2a2c4bce1539283b6ba5320e494c92f8d1
D3fau4 <raulwii2@gmail.com>
2022-03-19 17:00:32 +0100

    Crear archivos romfs y comprimidos con lz4

--- Unity.Mara.Lib/Assets/Mara.Lib/Editor/Setup/SetupEditor.cs
+++ Unity.Mara.Lib/Assets/Mara.Lib/Editor/Setup/SetupEditor.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
 using UnityEditor;
+using LibHac.FsSystem.RomFs;
+using LibHac.FsSystem;
+using LibHac.Fs;
+using System.IO;
+using K4os.Compression.LZ4.Streams;
 
 namespace Unity.Mara.Lib
 {
     public class SetupEditor
     {
-        [MenuItem("Mara/Setup")]
-        private static void SetupMara()
+        [MenuItem("Mara/Build Romfs")]
+        private static void BuildRomfs()
         {
-            Debug.Log("SetupMara");
+            string path = EditorUtility.OpenFolderPanel("Selecciona Carpeta", "", "");
+            string output = EditorUtility.SaveFilePanel("Selecciona donde guarda el parche", "", "", "romfs");
+
+            var localFs = new LocalFileSystem(path);
+            RomFsBuilder builder = new RomFsBuilder(localFs);
+            IStorage romFs = builder.Build();
+
+            romFs.GetSize(out long romFsSize).ThrowIfFailure();
+            using (var target = LZ4Stream.Encode(File.Create(output + ".lz4"), K4os.Compression.LZ4.LZ4Level.L12_MAX))
+            {
+                romFs.CopyToStream(target, romFsSize);
+            }
         }
     }
-
 }
