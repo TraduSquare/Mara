61c6bf62b0985c66b115122b05d8809dd015898a
Raúl Álvarez <raulwii2@gmail.com>
2022-04-17 16:42:35 +0200

    Implementado checkeos la header

--- Mara.Lib/Platforms/PS3/IO/EDAT.cs
+++ Mara.Lib/Platforms/PS3/IO/EDAT.cs
@@ -24,7 +24,127 @@ namespace Mara.Lib.Platforms.PS3.IO
             else
             {
                 Console.WriteLine("NPD valid!");
+                EDAT_Data data = getEDATData(reader);
+                byte[] rifkey = getKey(npd, data, klicense, keyFromRif);
+                
+                if (rifkey == null) {
+                    throw new Exception("ERROR: Key for decryption is missing");
+                }
+                Console.WriteLine($"DECRYPTION KEY: {BitConverter.ToString(rifkey)}");
+
+                if (!checkHeader(rifkey, data, npd, reader))
+                {
+                    throw new Exception("Error verifying header.");
+                }
+
+                if (decryptData(reader, npd, data, rifkey))
+                {
+                    
+                }
             }
+        }
+
+        private static bool decryptData(DataReader reader, NPD npd, EDAT_Data data, byte[] rifkey)
+        {
+            int numBlocks = (int) ((data.fileLen + data.blockSize - 1L) / data.blockSize);
+            int metadataSectionSize = ((data.flags & 0x1L) != 0x0L || (data.flags & 0x20L) != 0x0L) ? 32 : 16;
+            int baseOffset = 256;
+            int keyIndex = 0;
+            
+            if (npd.Version == 4L) {
+                keyIndex = 1;
+            }
+
+            for (int i = 0; i < numBlocks; ++i)
+            {
+                reader.Stream.Seek(baseOffset + i * metadataSectionSize, SeekOrigin.Begin);
+                byte[] expectedHash = new byte[20];
+                int compressionEndBlock = 0;
+                long offset;
+                int len;
+
+                if ((data.flags & 0x1L) != 0x0L)
+                {
+                    
+                }
+            }
+            
+            throw new NotImplementedException();
+        }
+
+        private static bool checkHeader(byte[] rifkey, EDAT_Data data, NPD npd, DataReader reader)
+        {
+            reader.Stream.Seek(0, SeekOrigin.Begin);
+            byte[] header = new byte[160];
+            byte[] outBytes = new byte[160];
+            byte[] expectedHash = new byte[16];
+            
+            Console.WriteLine($"Checking NPD Version: {npd.Version}");
+            Console.WriteLine($"EDATA Flag: 0x{data.flags}");
+            
+            if (npd.Version == 0L || npd.Version == 1L) {
+                if ((data.flags & 0x7FFFFFFEL) != 0x0L) {
+                    return false;
+                }
+            } else if (npd.Version == 2L) {
+                if ((data.flags & 0x7EFFFFE0L) != 0x0L) {
+                    return false;
+                }
+            } else {
+                if (npd.Version != 3L && npd.Version != 4L)
+                {
+                    throw new Exception($"ERROR: VERSION {npd.Version} DETECTED");
+                }
+                if ((data.flags & 0x7EFFFFC0L) != 0x0L) {
+                    return false;
+                }
+            }
+            
+            if ((data.flags & 0x20L) != 0x0L && (data.flags & 0x1L) != 0x0L) {
+                return false;
+            }
+
+            header = reader.ReadBytes(header.Length);
+            expectedHash = reader.ReadBytes(expectedHash.Length);
+            
+            // Comprobar Hash
+            int hashFlag = ((data.flags & 0x8L) == 0x0L) ? 2 : 268435458;
+            if ((data.flags & 0x80000000L) != 0x0L) {
+                hashFlag |= 0x1000000;
+            }
+            int keyIndex = 0;
+            if (npd.Version == 4L) {
+                keyIndex = 1;
+            }
+            // TODO: Cosas que no entiendo
+            return true;
+        }
+
+        private static byte[] getKey(NPD npd, EDAT_Data data, byte[] klicense, byte[] keyFromRif)
+        {
+            byte[] result = null;
+            
+            if ((data.flags & 0x1000000L) != 0x0L)
+            {
+                result = new byte[16];
+                result = Utils.XOR(npd.DevHash, EDAT_Keys.SDATKEY);
+            } 
+            else if (npd.License == 3L)
+            {
+                result = klicense;
+            } 
+            else if (npd.License == 2L)
+            {
+                result = keyFromRif;
+            }
+
+            return result;
+        }
+
+        private static EDAT_Data getEDATData(DataReader reader)
+        {
+            reader.Stream.Seek(128L, SeekOrigin.Begin);
+            return EDAT_Data.CreateEDAT_Data(reader);
         }
 
         public static NPD validateNPD(string filename, byte[] devKLic, DataReader reader)
