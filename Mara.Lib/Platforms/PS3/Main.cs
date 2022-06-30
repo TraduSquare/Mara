630910f81b4b41fb8a223d3a562003cad1df4028
Raúl Álvarez <raulwii2@gmail.com>
2022-06-30 19:55:43 +0200

    [PS3] Patch proccess skeleton

---
+++ Mara.Lib/Platforms/PS3/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Mara.Lib.Platforms.PS3;
+
+public class Main : PatchProcess
+{
+    public Main(string oriFolder, string outFolder, string filePath) : base(oriFolder, outFolder, filePath)
+    {
+        
+    }
+
+    public override (int, string) ApplyTranslation()
+    {
+        throw new NotImplementedException();
+    }
+}
