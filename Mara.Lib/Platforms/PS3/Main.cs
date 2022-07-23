293a590289c740b2eaa7d3e4ecf98b052d3de8e6
Raúl Álvarez <raulwii2@gmail.com>
2022-07-23 11:32:51 +0200

    [PS3] Cargar archivos

--- Mara.Lib/Platforms/PS3/Main.cs
+++ Mara.Lib/Platforms/PS3/Main.cs
@@ -1,16 +1,33 @@
 ﻿using System;
+using System.IO;
+using Mara.Lib.Platforms.PS3.IO;
 
 namespace Mara.Lib.Platforms.PS3;
 
 public class Main : PatchProcess
 {
-    public Main(string oriFolder, string outFolder, string filePath) : base(oriFolder, outFolder, filePath)
+    private string m_GamePath;
+    private string[] m_dlc;
+    private byte[] m_key = new byte[0x10];
+    public Main(string oriFolder, string outFolder, string filePath, string titleid, string RAPfile, byte[] DEVKEY) 
+        : base(oriFolder, outFolder, filePath)
     {
+        m_GamePath = Path.Combine("game", titleid);
+        m_dlc = Directory.GetFiles(Path.Combine(m_GamePath, "DLC"), "*.EDAT", SearchOption.AllDirectories);
+        if (RAPfile != "")
+            m_key = RAP.GetKey(RAPfile);
         
+        foreach (var f in m_dlc)
+        {
+            EDAT.decryptFile(f,Path.Combine(tempFolder,"DAT").Replace("EDAT","DAT"), DEVKEY, m_key);
+        }
     }
 
     public override (int, string) ApplyTranslation()
     {
+        var count = maraConfig.FilesInfo.ListXdeltaFiles.Length;
+        var files = maraConfig.FilesInfo;
+        
         throw new NotImplementedException();
     }
 }
