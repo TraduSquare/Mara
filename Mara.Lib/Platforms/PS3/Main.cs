4370bda1cb7d4149b12845426a5a6a285d9a03c6
Raúl Álvarez <raulwii2@gmail.com>
2022-08-04 13:16:25 +0200

    Update Main.cs

--- Mara.Lib/Platforms/PS3/Main.cs
+++ Mara.Lib/Platforms/PS3/Main.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Mara.Lib.Platforms.PS3.IO;
 
@@ -7,18 +8,20 @@ namespace Mara.Lib.Platforms.PS3;
 public class Main : PatchProcess
 {
     private string m_GamePath;
-    private string[] m_dlc;
+    private string[] m_encdlc;
     private byte[] m_key = new byte[0x10];
+    private List<string> m_decdlc = new List<string>();
     public Main(string oriFolder, string outFolder, string filePath, string titleid, string RAPfile, byte[] DEVKEY) 
         : base(oriFolder, outFolder, filePath)
     {
         m_GamePath = Path.Combine("game", titleid);
-        m_dlc = Directory.GetFiles(Path.Combine(m_GamePath, "DLC"), "*.EDAT", SearchOption.AllDirectories);
+        m_encdlc = Directory.GetFiles(Path.Combine(m_GamePath, "DLC"), "*.EDAT", SearchOption.AllDirectories);
         if (RAPfile != "")
             m_key = RAP.GetKey(RAPfile);
         
-        foreach (var f in m_dlc)
+        foreach (var f in m_encdlc)
         {
+            m_decdlc.Add(Path.Combine(tempFolder,"DAT").Replace("EDAT","DAT"));
             EDAT.decryptFile(f,Path.Combine(tempFolder,"DAT").Replace("EDAT","DAT"), DEVKEY, m_key);
         }
     }
@@ -27,7 +30,48 @@ public class Main : PatchProcess
     {
         var count = maraConfig.FilesInfo.ListXdeltaFiles.Length;
         var files = maraConfig.FilesInfo;
+
+        var MANUAL = Path.Combine(outFolder, "MANUAL");
+        var game = Path.Combine(outFolder, "USRDIR");
+        
+        InitDirs(MANUAL, game);
+
+        for (var i = 0; i < count; i++)
+        {
+            var oriFile = $"{oriFolder}{Path.DirectorySeparatorChar}{files.ListOriFiles[i]}";
+            var xdelta = $"{tempFolder}{Path.DirectorySeparatorChar}{files.ListXdeltaFiles[i]}";
+            
+            var outFile = $"{game}{Path.DirectorySeparatorChar}{files.ListOriFiles[i]}";
+            var folderFile = Path.GetDirectoryName(outFile);
+            if (!Directory.Exists(folderFile))
+                Directory.CreateDirectory(folderFile);
+            
+            var resultApplyXdelta = ApplyXdelta(oriFile, xdelta, outFile, files.ListMd5Files[i]);
+
+            if (resultApplyXdelta.Item1 != 0)
+                return resultApplyXdelta;
+        }
+        
+        // Copy Files
+        for (int i = 0; i < files.ListCopyFiles.Length; i++)
+        {
+            var oriFile = $"{tempFolder}{Path.DirectorySeparatorChar}{files.ListCopyFiles[i]}";
+            var outFile = $"{game}{Path.DirectorySeparatorChar}{files.ListCopyFiles[i]}";
+            var folderFile = Path.GetDirectoryName(outFile);
+            if (!Directory.Exists(folderFile))
+                Directory.CreateDirectory(folderFile);
+            
+            File.Copy(oriFile, outFile);
+        }
         
         throw new NotImplementedException();
+    }
+
+    private void InitDirs(string p1, string p2)
+    {
+        if (!Directory.Exists(p1))
+            Directory.CreateDirectory(p1);
+        if (!Directory.Exists(p2))
+            Directory.CreateDirectory(p2);
     }
 }
