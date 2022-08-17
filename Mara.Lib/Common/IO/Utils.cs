2d0be8a8eee727a48fd614cdddea9a957c64d72f
Raúl Álvarez <raulwii2@gmail.com>
2022-08-17 19:31:14 +0200

    Update Utils.cs

--- Mara.Lib/Common/IO/Utils.cs
+++ Mara.Lib/Common/IO/Utils.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using K4os.Compression.LZ4;
-using LibHac.Fs;
 using LibHac.FsSystem;
 using LibHac.Tools.FsSystem;
 using LibHac.Tools.FsSystem.RomFs;
@@ -38,10 +37,11 @@ public class Utils
             {
                 var target = new byte[LZ4Codec.MaximumOutputSize(owo.m_size)];
                 _writer.Write(owo.m_size);
-                _writer.Write(LZ4Codec.Encode(owo.m_data, 0, owo.m_size, target, 0, target.Length, LZ4Level.L12_MAX));
-                File.WriteAllBytes("ad", owo.m_data);
-                File.WriteAllBytes("dsa", target);
-                _writer.Write(target);
+                var size = LZ4Codec.Encode(owo.m_data, 0, owo.m_size, target, 0, target.Length, LZ4Level.L12_MAX);
+                _writer.Write(size);
+                var data_con = new byte[size];
+                Array.Copy(target, data_con, size);
+                _writer.Write(data_con);
             }
             else
             {
@@ -68,10 +68,10 @@ public class Utils
         m_file.m_version = _reader.ReadInt32();
         m_file.m_compressed = _reader.ReadByte();
         m_file.m_numEntrys = _reader.ReadInt32();
-        List<OWO> m_OWOS = new List<OWO>();
+        var m_OWOS = new List<OWO>();
         for (var i = 0; i < m_file.m_numEntrys; i++)
         {
-            OWO o = new OWO();
+            var o = new OWO();
             var owo_magic = _reader.ReadInt32();
             if (owo_magic != OWO.m_magic)
                 throw new Exception("Sistema de archivo (OWO) no reconocido.");
@@ -79,7 +79,7 @@ public class Utils
             o.m_size = _reader.ReadInt32();
             o.m_compressedsize = _reader.ReadInt32();
             o.m_data = _reader.ReadBytes(o.m_compressedsize);
-            
+
             // Descomprimir si es necesario
             if (m_file.m_compressed == 1)
             {
@@ -89,25 +89,26 @@ public class Utils
                     target, 0, target.Length);
                 o.m_data = target;
             }
+
             m_OWOS.Add(o);
         }
 
         /*var footer = _reader.ReadString();
         if (!footer.Equals(UWU.m_footer))
             throw new Exception("Firma del archivo incorrecta");*/
-        
+
         m_file.m_entry = m_OWOS.ToArray();
-        
+
         return m_file;
     }
 
     public static void WriteOWO(string path)
     {
         var localFs = new LocalFileSystem(path);
-        RomFsBuilder builder = new RomFsBuilder(localFs);
-        IStorage romFs = builder.Build();
+        var builder = new RomFsBuilder(localFs);
+        var romFs = builder.Build();
 
-        romFs.GetSize(out long romFsSize).ThrowIfFailure();
+        romFs.GetSize(out var romFsSize).ThrowIfFailure();
 
         using (var outFile = new FileStream("output.owo", FileMode.Create, FileAccess.ReadWrite))
         {
