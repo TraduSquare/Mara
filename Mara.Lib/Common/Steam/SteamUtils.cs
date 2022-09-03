0a7c2c6cb97aa5c6b7e123aff321ede56e95fac8
Raúl Álvarez <raulwii2@gmail.com>
2022-09-03 11:38:20 +0200

    Refactor

--- Mara.Lib/Common/Steam/SteamUtils.cs
+++ Mara.Lib/Common/Steam/SteamUtils.cs
@@ -4,87 +4,15 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using Microsoft.Win32;
-using Yarhl.IO;
 
-namespace Mara.Lib.Common;
+namespace Mara.Lib.Common.Steam;
 
-/// <summary>
-///     Extrae el listado de appsIDs de los juegos de steam
-/// </summary>
-internal class PackageInfo
-{
-    public DataReader _reader;
-    public List<int> AppID = new();
-
-    public PackageInfo(string path)
-    {
-        if (File.Exists(path))
-            _reader = new DataReader(DataStreamFactory.FromFile(path, FileOpenMode.Read));
-        else
-            throw new Exception("Archivo no encontrado");
-
-        while (true)
-        {
-            var p = Search(_reader.Stream, new byte[] { 0x61, 0x70, 0x70, 0x69, 0x64, 0x73, 0x00, 0x02, 0x30, 0x00 });
-            if (p != -1)
-                AppID.Add(_reader.ReadInt32());
-            else
-                break;
-        }
-    }
-
-    private static long Search(Stream stream, byte[] pattern)
-    {
-        long start = -1;
-
-        while (stream.Position < stream.Length)
-        {
-            if (stream.ReadByte() != pattern[0])
-                continue;
-
-            start = stream.Position - 1;
-
-            for (var idx = 1; idx < pattern.Length; idx++)
-                if (stream.ReadByte() != pattern[idx])
-                {
-                    start = -1;
-                    break;
-                }
-
-            if (start > -1) return start;
-        }
-
-        return start;
-    }
-}
-
-internal class LibraryFolders
-{
-    public List<string> SteamLibraries = new();
-
-    public LibraryFolders(string path)
-    {
-        if (File.Exists(path))
-        {
-            var txt = File.ReadAllLines(path);
-
-            foreach (var s in txt)
-                if (s.Contains("\"path\""))
-                    SteamLibraries.Add(s.Replace("		\"path\"		\"", "").Replace("\"", ""));
-        }
-        else
-        {
-            throw new Exception("Archivo no encontrado");
-        }
-    }
-}
-
-public class Steam
+public class SteamUtils
 {
     private readonly PackageInfo packageinfo;
     private readonly LibraryFolders SteamGameLibraries;
 
-    public Steam()
+    public SteamUtils()
     {
         // deberia ser igual en todos lados desconozco si en Deck es diferente
         packageinfo = new PackageInfo(Path.Combine(GetSteamPath(), "appcache", "packageinfo.vdf"));
