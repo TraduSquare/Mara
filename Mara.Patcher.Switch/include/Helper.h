2929c41240b7e1aabd86110cede6dc2812b27559
Raúl Álvarez <raulwii2@gmail.com>
2023-01-13 22:05:05 +0100

    Working xdelta3

---
+++ Mara.Patcher.Switch/include/Helper.h
@@ -0,0 +1,7 @@
+#ifndef XDELTA3_NX_HELPER_H
+#define XDELTA3_NX_HELPER_H
+
+#include <iostream>
+ int patch(std::string inPath, std::string outPath, std::string patchPath);
+
+#endif //XDELTA3_NX_HELPER_H
