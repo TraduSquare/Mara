6b6d2b37b12c7683a1e65f3395fdd1dd6d5eaab2
Raúl Álvarez <raulwii2@gmail.com>
2023-06-24 21:32:34 +0200

    56hsytrgfhtrgfetdrzg

---
+++ Unity.Mara.Lib/Assets/Mara.Lib/Runtime/Platforms/Generic/MainAot.cs
@@ -0,0 +1,80 @@
+using System;
+using System.IO;
+using System.Linq;
+using Mara.Lib.Common.IO;
+using UnityEngine;
+
+namespace Mara.Lib.Platforms.Generic
+{
+    public class MainAot : PatchProcess
+    { 
+        public MainAot(string oriFolder, string outFolder, string filePath) : base(oriFolder, outFolder, filePath)
+        {
+        }
+        
+        public MainAot(string oriFolder, string outFolder, OWO filePath) : base(oriFolder, outFolder, filePath)
+        {
+        }
+
+        public override (int, string) ApplyTranslation()
+        {
+            var count = maraConfig.FilesInfo.ListXdeltaFiles.Length;
+            var files = maraConfig.FilesInfo;
+
+            for (int i = 0; i < count; i++)
+            {
+                var excludeFile = false;
+
+                if (files.ListExcludeFiles != null)
+                    excludeFile = CheckExcludeFile(files.ListOriFiles[i]);
+
+                var oriFile = $"{oriFolder}{Path.DirectorySeparatorChar}{files.ListOriFiles[i].Replace('\\', Path.DirectorySeparatorChar)}";
+                var xdelta = $"{tempFolder}{Path.DirectorySeparatorChar}{files.ListXdeltaFiles[i].Replace('\\', Path.DirectorySeparatorChar)}";
+
+                if (excludeFile)
+                    if (!File.Exists(oriFile))
+                        continue;
+
+                var result = ApplyXdelta(oriFile, xdelta, oriFile, files.ListMd5Files[i]);
+
+                if (result.Item1 != 0 && !checkDLC(oriFile))
+                    return result;
+            }
+            
+            // Copy Files
+            for (var i = 0; i < files.ListCopyFiles.Length; i++)
+            {
+                var oriFile = $"{tempFolder}{Path.DirectorySeparatorChar}{files.ListCopyFiles[i]}";
+                var outFile = $"{oriFolder}{Path.DirectorySeparatorChar}{files.ListCopyFiles[i]}";
+                var folderFile = Path.GetDirectoryName(outFile);
+
+                if (!Directory.Exists(folderFile))
+                    Directory.CreateDirectory(folderFile);
+                
+                if(File.Exists(outFile))
+                    File.Delete(outFile);
+                
+                File.Copy(oriFile, outFile);
+            }
+            
+            return base.ApplyTranslation();
+        }
+
+        private bool CheckExcludeFile(string file)
+        {
+            foreach (var excludeFile in maraConfig.FilesInfo.ListExcludeFiles)
+            {
+                if (excludeFile == file)
+                    return true;
+            }
+
+            return false;
+        }
+        
+        private bool checkDLC(string path)
+        {
+            var info = new DirectoryInfo(Path.GetDirectoryName(path));
+            return info.Name.ToUpper().Contains("DLC");
+        }
+    }
+}
